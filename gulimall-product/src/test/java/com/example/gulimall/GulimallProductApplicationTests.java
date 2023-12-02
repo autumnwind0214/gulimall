28cb03a532e87b092b7f945f4193750ac26cf477
Huang <1174504109@qq.com>
2023-12-02 19:04:00 +0800

    P63:商品服务-API-品牌管理-OSS获取服务端签名(ps:配置文件读取未完成)

--- gulimall-product/src/test/java/com/example/gulimall/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/example/gulimall/GulimallProductApplicationTests.java
@@ -4,23 +4,12 @@ import com.example.gulimall.product.entity.BrandEntity;
 import com.example.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
 
-    // @Value("${spring.cloud.alicloud.access-key}")
-    private String accessKey;
 
-    @Value("${spring.cloud.alicloud.secret-key}")
-    private String secretKey;
-
-    @Value("${spring.cloud.alicloud.oss.endpoint}")
-    private String endpoint;
-
-    @Value("${spring.cloud.alicloud.oss.bucket}")
-    private String bucket;
 
     @Autowired
     BrandService brandService;
@@ -32,12 +21,6 @@ class GulimallProductApplicationTests {
         brandService.save(brandEntity);
     }
 
-    @Test
-    void test() {
-        System.out.println(accessKey);
-        System.out.println(secretKey);
-        System.out.println(endpoint);
-        System.out.println(bucket);
-    }
+
 
 }
--- gulimall-third-party/src/test/java/com/example/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
+++ gulimall-third-party/src/test/java/com/example/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -1,8 +1,9 @@
 package com.example.gulimall.thirdparty;
 
-import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSS;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.FileInputStream;
@@ -12,8 +13,20 @@ import java.io.InputStream;
 @SpringBootTest
 class GulimallThirdPartyApplicationTests {
 
+    @Value("${spring.cloud.alicloud.access-key}")
+    private String accessKey;
+
+    @Value("${spring.cloud.alicloud.secret-key}")
+    private String secretKey;
+
+    @Value("${spring.cloud.alicloud.oss.endpoint}")
+    private String endpoint;
+
+    @Value("${spring.cloud.alicloud.oss.bucket}")
+    private String bucket;
+
     @Autowired
-    OSSClient ossClient;
+    OSS ossClient;
 
     @Test
     public void testUpload() throws FileNotFoundException {
@@ -30,7 +43,11 @@ class GulimallThirdPartyApplicationTests {
     }
 
     @Test
-    void contextLoads() {
+    void test() {
+        System.out.println(accessKey);
+        System.out.println(secretKey);
+        System.out.println(endpoint);
+        System.out.println(bucket);
     }
 
 }
